c4bcab46936e08efde911a56c69994778a23b460
Hugo S. Dias <hugo_sdias@hotmail.com>
2022-10-08 09:22:59 -0300

    push

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+//item majoritario em um array.
+public class Program
+{
+    public static void Main(String[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+        
+        int[] num = new int[n];
+    
+// TODO: Crie as outras condições necessárias para a resolução do desafio:
+        for (int i = 0; i < n; i++)
+        {
+            num[i] = int.Parse(Console.ReadLine());
+        }
+        Console.WriteLine(MajorityElement(num));
+        
+    }
+    public static int MajorityElement(int[] nums)
+    {
+        int major = nums[0];
+        int count = 1;
+        for ( int i = 0; i < nums.Length; i++)
+        {
+            if (nums[i] != major)
+            {
+                major = nums[i];
+                count++;
+            }
+            else
+            {
+                if (major == nums[i])
+                {
+                    count++;
+                }
+                else
+                {
+                    count--;
+                }
+            }
+        }
+        return major;
+    }
+}   
